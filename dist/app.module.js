a88b1a818a538b38546606bbb6ede923b834f424
oksinlove <a.smile.of.fortune@gmail.com>
2017-03-08 00:54:06 +0200

    Init

--- dist/app.module.js
+++ dist/app.module.js
@@ -5,14 +5,10 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
 var platform_browser_1 = require("@angular/platform-browser");
-var forms_1 = require("@angular/forms");
-var app_routing_1 = require("./app.routing");
 var app_component_1 = require("./app.component");
-var home_component_1 = require("./home/home.component");
-var contact_component_1 = require("./contact/contact.component");
-var not_found_component_1 = require("./not-found/not-found.component");
 var AppModule = (function () {
     function AppModule() {
     }
@@ -21,17 +17,11 @@ var AppModule = (function () {
 AppModule = __decorate([
     core_1.NgModule({
         imports: [
-            platform_browser_1.BrowserModule,
-            forms_1.FormsModule,
-            app_routing_1.appRouting
+            platform_browser_1.BrowserModule
         ],
         declarations: [
             app_component_1.AppComponent,
-            home_component_1.HomeComponent,
-            contact_component_1.ContactComponent,
-            not_found_component_1.NotFoundComponent
         ],
-        providers: [],
         bootstrap: [app_component_1.AppComponent]
     })
 ], AppModule);
